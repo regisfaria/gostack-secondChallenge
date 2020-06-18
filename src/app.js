d2fe498057d507eda25a466c7c045206a8ce8be5
regisfaria <rgisfaria@yahoo.com.br>
2020-06-17 23:19:34 -0300

    finished the challenge requisites

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -9,25 +8,73 @@ app.use(express.json());
 app.use(cors());
 
 const repositories = [];
+/*
+function isValidId(request, response, next) {
+  const { id } = request.body;
+}*/
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get("/repositories", (request, response) => {       // OK
+  //const {} = request.query;
+
+  //const results = owner ? projects.filter(project => project.owner == owner) : projects;
+  
+  return response.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post("/repositories", (request, response) => {        // OK
+  const {title, url, techs } = request.body;
+
+  const repository =  { id: uuid(), title, url, techs, likes: 0};
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", (request, response) => {     // OK
+  const { id } = request.params;
+  const { title, url, techs} = request.body;
+
+  const rI = repositories.findIndex(repository => repository.id === id);
+
+  if (rI < 0) {
+    return response.status(400).json({error: "No repository was found with the given ID"});
+  }
+
+  repositories[rI].title = title;
+  repositories[rI].url = url;
+  repositories[rI].techs = techs;
+
+  return response.json(repositories[rI]);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {    // OK
+  const { id } = request.params;
+
+  const rI = repositories.findIndex(repository => repository.id === id);
+
+  if (rI < 0) {
+    return response.status(400).json({error:"Repository not found"});
+  }
+
+  repositories.splice(rI, 1);
+
+  // At deletion, we usually return a send response, with status 204
+  return response.status(204).send(); 
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", (request, response) => {   // OK
+  const { id } = request.params;
+
+  const rI = repositories.findIndex(repository => repository.id === id);
+
+  if (rI < 0) {
+    return response.status(400).json({error:"Repository not found"});
+  }
+
+  repositories[rI].likes++;
+
+  return response.json(repositories[rI]);
 });
 
 module.exports = app;
